c5c38e95d558d431e921deeafbf76ec4449369c2
Micah Walter <micah@micahwalter.com>
2019-03-22 11:28:14 -0400

    init

---
+++ README.md
@@ -0,0 +1,40 @@
+# Tessitura
+
+Tools for talking to the Tessitura API from Python. This is all just wet paint at the moment.
+
+Requires Python 3.6+
+
+To install just do:
+
+```
+$ pip install tessitura
+```
+
+To use see the following example:
+
+```
+import tessitura
+import pprint
+
+endpoint = "https://your.tessitura-endpoint.org/TessituraService/"
+credentials = "username:group:location:password"
+
+# The REST API method you wish to call. This may need to be created with other information depending on the request, such as a session id -- See Tessitura documentation for more information. Please note there should be no trailing slash at the end.
+
+method = "Diagnostics/Status"
+
+# The type of request you wish to make. Currently supports GET and POST.
+
+request_type = "GET"
+
+# Use this to add any query string parameters to a GET request
+params = {}
+
+# Use this to add any form data to a POST request
+data = {}
+
+# Call the API
+rsp = tessitura.rest_call(endpoint, credentials, request_type, method, params=params, data=data)
+
+# Print out the results in JSON format
+pprint.pprint(rsp)
---
+++ tessitura/__init__.py
@@ -0,0 +1,60 @@
+__import__('pkg_resources').declare_namespace(__name__)
+
+import requests
+
+import json
+import base64
+
+def rest_call(endpoint, credentials, request_type, method, *args, **kwargs):
+
+    params = kwargs.get('params', None)
+    data = kwargs.get('data', None)
+
+    url = endpoint + method
+
+    r = {}
+   
+    headers = {
+        'Accept': "application/json",
+        'Content-Type': "application/json",
+        'Authorization': api_prepare_auth_string(credentials)
+    }
+
+    try:
+        if request_type == "GET":
+            r = requests.get(url, params=params, headers=headers, timeout=15)
+        elif request_type == "POST":
+            r = requests.request("POST", url, data=str(data), headers=headers, timeout=15)
+    
+    except Exception as e:
+        
+        timeout = {
+            "error": 500,
+            "message": "Error connecting to Tessitura",
+            "e": e
+        }
+
+        return timeout
+    
+    j = r.json()
+
+    if r.status_code != 200:
+        
+        j = {
+            "error": r.status_code,
+            "message": j[0]['Description']
+        }
+    
+    return j
+
+
+def api_prepare_auth_string(credentials):
+
+    encoded = base64.b64encode(str.encode(credentials))
+    encoded = str(encoded.decode())
+    authHeader = 'Basic ' + encoded
+
+    return authHeader
+
+
+
